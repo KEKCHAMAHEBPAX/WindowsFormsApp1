63d5e6dea867a2b37c486aa52bd1fbdeb29c3723
КондратьевСорокин Арсений <aasorokin74@yandex.ru>
2022-02-05 12:16:55 +0300

    b

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-        int a;
+        int a, b;
         private void Form1_Load(object sender, EventArgs e)
         {
            
